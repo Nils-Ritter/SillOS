8cd740ed5d11d6189ca504c11302e772482f7334
Nils Ritter <ritter.nils@protonmail.com>
2026-08-06 21:33:43 +0200

    Basic test framework

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
@@ -16,7 +19,9 @@ pub extern "C" fn _start() -> ! {
 /// The main kernel function
 /// this ever returning will halt the cpu forever.
 fn k_main(){
-    panic!("Kernel execution is over!")
+    println!("[INFO] Welcome to SillOS!");
+    #[cfg(test)]
+    test_main();
 }
 
 /// This function is called on panic.
@@ -24,4 +29,19 @@ fn k_main(){
 fn panic(info: &PanicInfo) -> ! {
     print!("[FATAL] {}", info);
     loop {}
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("[TESTS] Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion(){
+    print!("[TESTS] trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
